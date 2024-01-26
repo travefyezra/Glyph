01cd7dea04e6ce199dfdc4f8e6740a8d39452789
Ezra Zinberg <ezra@travefy.com>
2024-01-26 15:01:29 -0500

    Consolidate code and fix backspace bug

--- Wordle/js/script.js
+++ Wordle/js/script.js
@@ -2,7 +2,7 @@ var htmlElements = [];
 var answer = "";
 var wordString = "About,Alert,Argue,Beach,Above,Alike,Arise,Began,Abuse,Alive,Array,Begin,Actor,Allow,Aside,Begun,Acute,Alone,Asset,Being,Admit,Along,Audio,Below,Adopt,Alter,Audit,Bench,Adult,Among,Avoid,Billy,After,Anger,Award,Birth,Again,Angle,Aware,Black,Agent,Angry,Badly,Blame,Agree,Apart,Baker,Blind,Ahead,Apple,Bases,Block,Alarm,Apply,Basic,Blood,Album,Arena,Basis,Board,Boost,Buyer,China,Cover,Booth,Cable,Chose,Craft,Bound,Calif,Civil,Crash,Brain,Carry,Claim,Cream,Brand,Catch,Class,Crime,Bread,Cause,Clean,Cross,Break,Chain,Clear,Crowd,Breed,Chair,Click,Crown,Brief,Chart,Clock,Curve,Bring,Chase,Close,Cycle,Broad,Cheap,Coach,Daily,Broke,Check,Coast,Dance,Brown,Chest,Could,Dated,Build,Chief,Count,Dealt,Built,Child,Court,Death,Debut,Entry,Forth,Group,Delay,Equal,Forty,Grown,Depth,Forum,Guard,Doing,Event,Found,Guess,Doubt,Every,Frame,Guest,Dozen,Exact,Frank,Guide,Draft,Exist,Fraud,Happy,Drama,Extra,Fresh,Harry,Drawn,Faith,Front,Heart,Dream,False,Fruit,Heavy,Dress,Fault,Fully,Hence,Drill,Fibre,Funny,Night,Drink,Field,Giant,Horse,Drive,Fifth,Given,Hotel,Drove,Fifty,Glass,House,Dying,Fight,Globe,Human,Eager,Final,Going,Ideal,Early,First,Grace,Image,Earth,Fixed,Grade,Index,Eight,Flash,Grand,Inner,Elite,Fleet,Grant,Input,Empty,Floor,Grass,Issue,Enemy,Fluid,Great,Irony,Enjoy,Focus,Green,Juice,Enter,Force,Gross,Joint,Judge,Metal,Media,Newly,Known,Local,Might,Noise,Label,Logic,Minor,North,Large,Loose,Minus,Noted,Laser,Lower,Mixed,Novel,Later,Lucky,Model,Nurse,Laugh,Lunch,Money,Occur,Layer,Lying,Month,Ocean,Learn,Magic,Moral,Offer,Lease,Major,Motor,Often,Least,Maker,Mount,Order,Leave,March,Mouse,Other,Legal,Music,Mouth,Ought,Level,Match,Movie,Paint,Light,Mayor,Needs,Paper,Limit,Meant,Never,Party,Peace,Power,Radio,Round,Panel,Press,Raise,Route,Phase,Price,Range,Royal,Phone,Pride,Rapid,Rural,Photo,Prime,Ratio,Scale,Piece,Print,Reach,Scene,Pilot,Prior,Ready,Scope,Pitch,Prize,Refer,Score,Place,Proof,Right,Sense,Plain,Proud,Rival,Serve,Plane,Prove,River,Seven,Plant,Queen,Quick,Shall,Plate,Sixth,Stand,Shape,Point,Quiet,Roman,Share,Pound,Quite,Rough,Sharp,Sheet,Spare,Style,Times,Shelf,Speak,Sugar,Tired,Shell,Speed,Suite,Title,Shift,Spend,Super,Today,Shirt,Spent,Sweet,Topic,Shock,Split,Table,Total,Shoot,Spoke,Taken,Touch,Short,Sport,Taste,Tough,Shown,Staff,Taxes,Tower,Sight,Stage,Teach,Track,Since,Stake,Teeth,Trade,Sixty,Start,Texas,Treat,Sized,State,Thank,Trend,Skill,Steam,Theft,Trial,Sleep,Steel,Their,Tried,Slide,Stick,Theme,Tries,Small,Still,There,Truck,Smart,Stock,These,Truly,Smile,Stone,Thick,Trust,Smith,Stood,Thing,Truth,Smoke,Store,Think,Twice,Solid,Storm,Third,Under,Solve,Story,Those,Undue,Sorry,Strip,Three,Union,Sound,Stuck,Threw,Unity,South,Study,Throw,Until,Space,Stuff,Tight,Upper,Upset,Whole,Waste,Wound,Urban,Whose,Watch,Write,Usage,Woman,Water,Wrong,Usual,Train,Wheel,Wrote,Valid,World,Where,Yield,Value,Worry,Which,Young,Video,Worse,While,Youth,Virus,Worst,White,Worth,Visit,Would,Vital,Voice,Glyph";
 var wordsArray = [];
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var unguessedLetters = [];
 var chatMessage = "";
 var guessCount = 0;
@@ -69,12 +69,8 @@ function clearLastRow() {
     }
 }
 
-function handleBackspace() {
-    clearLastRow();
-    htmlElements.push(toElement("", SPACER));
-
-    currentInputValue = currentInputValue.substring(0,currentInputValue.length-1);
-    guessArr = currentInputValue.toUpperCase().split('');
+function updateRowFromInput(val) {
+    guessArr = val.toUpperCase().split('');
 
     for (var i = 0; i < guessArr.length; i++) {
         var letter = guessArr[i];
@@ -88,6 +84,16 @@ function handleBackspace() {
         }
     }
     htmlElements.push("<br/>");
+}
+
+function handleBackspace() {
+    var erasedLetter = currentInputValue.charAt(currentInputValue.length-1).toUpperCase();
+    if (erasedLetter != "") unguessedLetters.push(erasedLetter);
+    clearLastRow();
+    htmlElements.push(toElement("", SPACER));
+
+    currentInputValue = currentInputValue.substring(0,currentInputValue.length-1);
+    updateRowFromInput(currentInputValue);
 
     render();
 }
@@ -98,20 +104,7 @@ function handleInput(currentInput) {
     htmlElements.push(toElement("", SPACER));
 
     currentInputValue = currentInputValue + currentInput;
-    guessArr = currentInputValue.toUpperCase().split('');
-
-    for (var i = 0; i < guessArr.length; i++) {
-        var letter = guessArr[i];
-        htmlElements.push(toElement(letter, EMPTY));
-    }
-
-    var lettersNotGuessed = 5 - guessArr.length;
-    if (lettersNotGuessed > 0) {
-        for (var i = 0; i < lettersNotGuessed; i++) {
-            htmlElements.push(toElement("_", i == 0 ? CURSOR : EMPTY));
-        }
-    }
-    htmlElements.push("<br/>");
+    updateRowFromInput(currentInputValue);
 
     if (currentInputValue.length >= 5) {
         handleGuess(currentInputValue);
@@ -185,23 +178,6 @@ function handleGuess(currentGuess) {
     }
 
     guessElements.forEach(el => htmlElements.push(el));
-    
-
-    // for (var i = 0; i <  guessArr.length; i++) {
-    //     
-    //     var letter = guessArr[i];
-    //     if (letter == answer.charAt(i)) {
-    //         htmlElements.push(toElement(letter, CORRECT));
-    //         correctLetters = correctLetters + 1;
-    //     }
-    //     else if (answer.indexOf(letter) >= 0) {
-    //         htmlElements.push(toElement(letter, ALMOST));
-    //         almostLetters = almostLetters + 1;
-    //     }
-    //     else {
-    //         htmlElements.push(toElement(letter, WRONG));
-    //     }
-    // }
 
     htmlElements.push("<br/>");
 
@@ -239,7 +215,6 @@ function updateMessage(correct, almost) {
     if (correct >= 5) message = "You win!";
     else if (almost >= 2) message = "Making progress...";
     if (correct < 0) message = "You lose :(";
-    // htmlElements.push(toElement(message, 'p'));
     for (var i = 0; i < htmlElements.length; i++) {
         var elem = htmlElements[i];
         if (elem.charAt(1) == 'p') {
